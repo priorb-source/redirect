6c63423c15001ca861054ad54d68267fb02ba495
bitowaqr <bitowaqr@gmail.com>
2024-10-01 08:45:09 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
-  
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
+  },
 })
